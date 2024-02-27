26e20a0aff99ee2a8e89b1723294248a9b997d21
cassy25 <cgeoffra@PCassy>
2024-02-27 17:06:33 +0100

    change remove user from channel

--- srcs/Channel.hpp
+++ srcs/Channel.hpp
@@ -11,12 +11,13 @@ class Channel
     private:
         Channel();
         std::map<std::string, Client> _userList;
+        std::map<std::string, Channel> _channels;
         std::string _nameChannel;
 
     public:
         Channel(std::string nameChannel):
         ~Channel();
         std::string getName() const;
+        void removeUserFromChannel(const std::string &nameChannel, const std::string &username);
         void removeUser(const std::string &username);
-
 };
--- srcs/channel.cpp
+++ srcs/channel.cpp
@@ -20,7 +20,13 @@ std::string Channel::getName() const;
     return (_nameChannel)
 }
 
-void Channel::removeUser(const std::string &username)
+void removeUser(const std::string& username) {
+        _userList.erase(username);
+    }
+
+void Channel::removeUserFromChannel(const std::string &nameChannel, const std::string &username)
 {
-    _userList.erase(username);
+    std::map<std;;string, Channel>::iterator it = _channels.find(channelName);
+    if (it != _channels.end())
+        it->second.removeUser(username);
 }
